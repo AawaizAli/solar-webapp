21d271fb77e2fc4b94be3f2e474746236a1f8310
Aawaiz Ali <114678240+AawaizAli@users.noreply.github.com>
2024-07-10 01:45:29 +0500

    loginPage change

--- solar-cleaning/frontend/src/components/LoginPage.jsx
+++ solar-cleaning/frontend/src/components/LoginPage.jsx
@@ -1,133 +1,112 @@
-import React from "react";
-import { Form, Input, Button, Checkbox } from "antd";
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
-import { useDispatch, useSelector } from "react-redux";
-import { login } from "../features/auth/authSlice";
-import { Navigate } from "react-router-dom";
-import "./LoginPage.css";
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../features/auth/authSlice';
+import { Navigate } from 'react-router-dom';
+import './LoginPage.css';
 
 const LoginPage = () => {
-    const dispatch = useDispatch();
-    const loading = useSelector((state) => state.auth.loading);
-    const error = useSelector((state) => state.auth.error);
-    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
+  const error = useSelector((state) => state.auth.error);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
-    const onFinish = (values) => {
-        dispatch(login({ username: values.email, password: values.password }));
-    };
+  const onFinish = (values) => {
+    dispatch(login({ username: values.email, password: values.password }));
+  };
 
-    if (isAuthenticated) {
-        return <Navigate to="/" />;
-    }
+  if (isAuthenticated) {
+    return <Navigate to="/" />;
+  }
 
-    return (
-        <>
-            {/* Header and Navbar */}
-            <header className="header_section">
-                <div className="header_top">
-                    <div className="container-fluid">
-                        <div className="contact_nav">
-                            <a href="">
-                                <i
-                                    className="header-icon fa fa-phone"
-                                    aria-hidden="true"
-                                />
-                                <span>Call : 03153738555</span>
-                            </a>
-                            <a href="">
-                                <i
-                                    className="header-icon fa fa-envelope"
-                                    aria-hidden="true"
-                                />
-                                <span>Email : hawktuah@gmail.com</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="header_bottom">
-                    <div className="container-fluid">
-                        <nav className="navbar navbar-expand-lg custom_nav-container">
-                            <a className="navbar-brand" href="/">
-                                <span> SolarPod </span>
-                            </a>
-                            <button
-                                className="navbar-toggler"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target="#navbarSupportedContent"
-                                aria-controls="navbarSupportedContent"
-                                aria-expanded="false"
-                                aria-label="Toggle navigation">
-                                <span className=""> </span>
-                            </button>
-                            <div
-                                className="collapse navbar-collapse"
-                                id="navbarSupportedContent">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="/">
-                                            Home
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-            </header>
-
-            {/* Login Form */}
-            <div className="login-container">
-                <Form
-                    name="login"
-                    className="login-form"
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}>
-                    <Form.Item
-                        name="email"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Please input your Email!",
-                            },
-                        ]}>
-                        <Input
-                            prefix={
-                                <UserOutlined className="site-form-item-icon" />
-                            }
-                            placeholder="Email"
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Please input your Password!",
-                            },
-                        ]}>
-                        <Input.Password
-                            prefix={
-                                <LockOutlined className="site-form-item-icon" />
-                            }
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            className="login-form-button"
-                            loading={loading}>
-                            Log in
-                        </Button>
-                    </Form.Item>
-                    {error && <p style={{ color: "red" }}>{error}</p>}
-                </Form>
+  return (
+    <>
+      {/* Header and Navbar */}
+      <header className="header_section">
+        <div className="header_top">
+          <div className="container-fluid">
+            <div className="contact_nav">
+              <a href="">
+                <i className="fa fa-phone" aria-hidden="true" />
+                <span>Call : 03153738555</span>
+              </a>
+              <a href="">
+                <i className="fa fa-envelope" aria-hidden="true" />
+                <span>Email : drbhai@gmail.com</span>
+              </a>
             </div>
-        </>
-    );
+          </div>
+        </div>
+        <div className="header_bottom">
+          <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg custom_nav-container">
+              <a className="navbar-brand" href="/">
+                <span> SolarPod </span>
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span className=""> </span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/login">
+                      Login
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      {/* Login Form */}
+      <div className="login-container">
+        <Form
+          name="login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: 'Please input your Email!' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              placeholder="Password"
+            />
+          </Form.Item>
+          
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button" loading={loading}>
+              Log in
+            </Button>
+          </Form.Item>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+        </Form>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
