71e6efea4d1ab18003e7e35ae650a730ba03cd65
Hashir Khan <hashirahmedkhan123@gmail.com>
2024-10-01 23:33:19 +0500

    update

--- solar-cleaning/frontend/src/components/Client.jsx
+++ solar-cleaning/frontend/src/components/Client.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+const [loading, setLoading] = useState(false);
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
